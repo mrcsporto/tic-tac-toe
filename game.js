c814593da9288a8ab9163ab64025e7a4fd8fe01e
mrcsporto <mrcsporto@gmail.com>
2022-07-03 15:52:08 -0300

    upload to testing

--- game.js
+++ game.js
@@ -2,7 +2,10 @@ let classes = Array.from(document.querySelectorAll('.box'))
 let xTurn = []
 let circleTurn = []
 let currentPlayer = ''
+let winnerPlayer
 let hasWinner
+let seconds = 5 // seconds for HTML
+let foo // variable for clearInterval() function
 const winnerCombination =[
     [0, 1, 2],
     [3, 4, 5],
@@ -14,6 +17,7 @@ const winnerCombination =[
     [2, 4, 6]
 ]
 
+// function highlight players turn on the game.html
 function changeColor() {
     if(currentPlayer == 'x'){
         document.getElementById('MarkPlayerCircle').style.backgroundColor = '#88B2CC'
@@ -41,7 +45,7 @@ function changeColor() {
     }
 }
 
-// function to insert player's symbol on the document location
+// function to insert player's symbol into the local path
 function firstPlayer() {
     if(window.location.href.includes("#x")) {
         currentPlayer = 'x' 
@@ -50,12 +54,23 @@ function firstPlayer() {
     }
 }   firstPlayer()
     changeColor()
+    
+// Change the player turn by add the player's name into the div .box
+function changePlayer() {
+    if (currentPlayer == 'circle') {
+        currentPlayer = 'x'
+    } else {
+        currentPlayer = 'circle'
+    }
+}    
 
 
-// loop to look up in which box the player has clicked on the classes Array
+// The loop to look up in the Array Classes which .box the player has clicked 
 // Add the mark of the current player into the HTML class
+// the function will stop if has a winner
 for (let i = 0; i < classes.length; i++) {
     let boxElement = classes[i]
+//inputMark function is activated by an click event
     function inputMark() {
         if (hasWinner) {
             return
@@ -73,47 +88,63 @@ for (let i = 0; i < classes.length; i++) {
       
     }
 
-    // Get which exactly possition of the classes Array was clicked and push it into the circleTurn and xTurn Arrays
+    // Get the last index of the classes's Array was clicked and push it into the circleTurn and xTurn Arrays
     function countTurn() {
         if (currentPlayer == 'circle') {
             classIndex = classes.lastIndexOf(boxElement)
-            console.log(classIndex)
             circleTurn.push(classIndex)
         } else {
             classIndex = classes.lastIndexOf(boxElement)
-            console.log(classIndex)
             xTurn.push(classIndex)
         }
     }
     boxElement.addEventListener('click', inputMark, {once: true})
 }
 
-function changePlayer() {
-    if (currentPlayer == 'circle') {
-        currentPlayer = 'x'
-    } else {
-        currentPlayer = 'circle'
+// Check if is draw
+function checkDraw() {
+    if ((xTurn.length + circleTurn.length === 9)) {
+        document.getElementById('winner').style.display = 'block'
+        document.getElementById('restart').style.display = 'block'
+        document.getElementById('winner').style.backgroundColor = 'rgba(255, 255, 255, 0.3)'
+        document.getElementById('viewPort').style.filter = 'blur(6px)'
+        document.getElementById('winner').innerHTML = 'Its a tie!!'
+        countdownTimer()
     }
 }
 
-function checkTrue(counter) {
-    return winner.includes(counter)
+function showWinner() {
+    document.getElementById('winner').style.display = 'block'
+    document.getElementById('restart').style.display = 'block'
+    document.getElementById('winner').style.backgroundColor = 'rgba(255, 255, 255, 0.3)'
+    document.getElementById('viewPort').style.filter = 'blur(6px)'
+    document.getElementById('player').classList.add('player' + winnerPlayer)
+   
 }
 
+// function to return the 
+function checkTrue(countTrue) {
+    return winner.includes(countTrue)
+}
+
+// loop to obtained an Array newWinner by calling the function checkTrue() with map() method
+// This function will percurr the winner Array combination to check if the xTurn or circleTurn Arrays matches to some winning combination
+// winCount will filter into the newWinner Array only the true
+// If winCount has 3 or more true then will input true on hasWinner variable
 function checkWinner() {
     for (let j = 0; j < winnerCombination.length; j++) {
         winner = winnerCombination[j]
-
         checkTrue()
 
         if (xTurn.length >= 3) {
             const newWinner = xTurn.map(checkTrue)
-            // console.log(newWinner)
+            console.log(newWinner)
             const winCount = newWinner.filter(Boolean).length
-            // console.log(winCount)
             if (winCount >= 3) {
+                winnerPlayer = "X"
                 hasWinner = true
-                // showWinner()
+                showWinner()
+                countdownTimer()
             }
         }
         if (circleTurn.length >= 3) {
@@ -122,7 +153,9 @@ function checkWinner() {
 
             if (winCount >= 3) {
                 hasWinner = true
-                // showWinner()
+                winnerPlayer = "Circle"
+                showWinner()
+                countdownTimer()
             }
         }
     }
@@ -131,19 +164,30 @@ function checkWinner() {
     }
 }
 
-function checkDraw() {
-    if ((xTurn.length + circleTurn.length === 9)) {
-        document.getElementById('winner').style.display = 'block'
-        document.getElementById('winner').innerHTML = 'Its a tie!!'
+
+
+
+
+// Countdown timer for redirecting to another URL after several seconds
+function redirect() {
+    document.location.href = 'http://127.0.0.1:5500/index.html'
+}
+
+function updateSecs() {
+    document.getElementById("seconds").innerHTML = 'The game will restart in ' + seconds + ' seconds'
+    seconds--
+    if (seconds == -1) {
+        clearInterval(foo)
+        redirect()
     }
 }
 
-// function showWinner() {
-//     document.getElementById('winner').style.display = 'block'
-//     document.getElementById('winner').innerHTML = 'Player ' + currentPlayer + ' won!!'
-   
-// }
+function countdownTimer() {
+    foo = setInterval(function () {
+        updateSecs()
+    }, 1000)
+}
 
-// function redirect() {
-//     document.location.href = 'http://127.0.0.1:5500/index.html';
-// }
+document.getElementById("restart").onclick = function () {
+    location.href = "http://127.0.0.1:5500/index.html"
+}
